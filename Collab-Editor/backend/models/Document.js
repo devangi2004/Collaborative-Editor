3e2621a090ac9e62e701bd58fc4e1d27e127eb1b
Devangi Inani <139806614+devangi2004@users.noreply.github.com>
2025-07-28 10:37:39 +0530

    Delete Collab-Editor directory

--- Collab-Editor/backend/models/Document.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const DocumentSchema = new mongoose.Schema({
-  _id: String,
-  data: Object
-});
-
-module.exports = mongoose.model("Document", DocumentSchema);
