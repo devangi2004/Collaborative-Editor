6060fd5f71ea37c5eee1e8c2f185691163a1c339
Devangi Inani <139806614+devangi2004@users.noreply.github.com>
2025-07-30 16:50:16 +0530

    Delete Collab-Editor directory

--- Collab-Editor/frontend/src/components/SaveStatus.js
+++
@@ -1,6 +0,0 @@
-
-const SaveStatus = ({ status }) => {
-  return <div style={{ fontSize: "14px", color: "gray" }}>{status}</div>;
-};
-
-export default SaveStatus;
--- Collab-Editor/frontend/src/context/authContext.js
+++
@@ -1,7 +0,0 @@
-import { createContext } from "react";
-
-const AuthContext = createContext({
-  user: { username: "Guest" },
-});
-
-export default AuthContext;
